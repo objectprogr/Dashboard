f780973500cdbcf316769a68b11ad51088806b70
objectprogr <objectprogr@gmail.com>
2018-03-06 14:29:00 +0100

    Added home and blog pages, routing, navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
+import { Routes,RouterModule } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+
+export const ROUTES: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'blog', component: BlogComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES),
   ],
   providers: [],
   bootstrap: [AppComponent]
